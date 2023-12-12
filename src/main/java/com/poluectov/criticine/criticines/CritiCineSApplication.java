254519fb67352591f6971657d172b1676df74f18
l9sik <hotsot.by@gmail.com>
2023-12-12 09:58:32 +0300

    init commit

---
+++ src/main/java/com/poluectov/criticine/criticines/CritiCineSApplication.java
@@ -0,0 +1,13 @@
+package com.poluectov.criticine.criticines;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CritiCineSApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CritiCineSApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/poluectov/criticine/criticines/CritiCineSApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poluectov.criticine.criticines;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CritiCineSApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
